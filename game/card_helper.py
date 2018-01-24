75b823e3a7f7168af615b7d2aaf6aa153f21fb37
Cynthia Sadler <cynthiasadler@cynthias-air.attlocal.net>
2018-01-24 09:17:27 -0600

    better handling of utf-8 cruft

--- game/card_helper.py
+++ game/card_helper.py
@@ -1,15 +1,26 @@
+# coding=utf-8
 from pymongo import MongoClient
+import doctest
 
 client = MongoClient()
 db = client.pokemontcg
 
 
 def normalize_damage(damage):
+    """
+    >>> normalize_damage("60+")
+    '60'
+    >>> normalize_damage("120−")
+    '120'
+    """
+    damage = damage.strip()
+    damage = damage.encode('ascii', 'ignore').decode()
     damage = damage.replace('+', '')
     damage = damage.replace('-', '')
     damage = damage.replace('x', '')
     damage = damage.replace('×', '')
     damage = damage.replace('n/a', '')
+
     return damage
 
 
